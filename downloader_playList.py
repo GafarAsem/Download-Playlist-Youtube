79e967c261a6654c2e90e51491659800e9c34202
gafarasem <gafarasem255@hotmail.com>
2021-06-07 08:59:32 -0800

    solve errors

--- downloader_playList.py
+++ downloader_playList.py
@@ -7,12 +7,10 @@ playListTap.get(playlist)
 numberOFvideos = playListTap.find_element_by_xpath('//*[@id="stats"]/yt-formatted-string[1]/span[1]').text
 print(numberOFvideos)
 
-
-
 downloadTap = webdriver.Chrome("chromedriver.exe")
 
-listVideos=[]
-errorListVideos=[]
+listVideos = []
+errorListVideos = []
 
 for video in playListTap.find_elements_by_xpath('//*[@id="video-title"]'):
     listVideos.append(video.get_attribute('href').split('&list')[0])
@@ -29,12 +27,11 @@ for i in range(len(listVideos)):
         downloadTap.find_element_by_xpath('//*[@id="sf_result"]/div/div/div[2]/div[2]/div[1]').click()
         time.sleep(1)
 
-        # error
-        print(str(i+1)+" done")
+        print(str(i + 1) + " done")
     except:
         print(listVideos[i])
         errorListVideos.append(listVideos[i])
-        print(i+1)
+        print(i + 1)
 
 time.sleep(300)
 for i in range(len(errorListVideos)):
@@ -49,7 +46,10 @@ for i in range(len(errorListVideos)):
         time.sleep(1)
 
         # error
-        print(str(i+1)+" done")
+        print(str(i + 1) + " done")
     except:
         print(errorListVideos[i])
-        print(i+1)
+        print(i + 1)
+
+downloadTap.quit()
+exit()
