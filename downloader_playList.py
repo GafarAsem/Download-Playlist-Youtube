f85c0866625ddd54c2566024d3c276606f368b53
gafarasem <gafarasem255@hotmail.com>
2021-06-07 02:43:41 -0800

    solve errors

--- downloader_playList.py
+++ downloader_playList.py
@@ -2,23 +2,54 @@ import time
 from selenium import webdriver
 
 playlist = input('url of playlist : ')
-web = webdriver.Chrome("chromedriver.exe")
-web.get(playlist)
-numberOFvideos = web.find_element_by_xpath('//*[@id="stats"]/yt-formatted-string[1]/span[1]').text
+playListTap = webdriver.Chrome("chromedriver.exe")
+playListTap.get(playlist)
+numberOFvideos = playListTap.find_element_by_xpath('//*[@id="stats"]/yt-formatted-string[1]/span[1]').text
 print(numberOFvideos)
 
-one = web.find_elements_by_xpath('//*[@id="video-title"]')
 
-web2 = webdriver.Chrome("chromedriver.exe")
 
-for i in range(len(one)):
+downloadTap = webdriver.Chrome("chromedriver.exe")
+
+listVideos=[]
+errorListVideos=[]
+
+for video in playListTap.find_elements_by_xpath('//*[@id="video-title"]'):
+    listVideos.append(video.get_attribute('href').split('&list')[0])
+
+playListTap.quit()
+for i in range(len(listVideos)):
     try:
-        web2.get('https://ar.savefrom.net/' + str(one[i].get_attribute('href')).split('&list')[0])
+        downloadTap.get('https://ar.savefrom.net/' + listVideos[i])
         time.sleep(2)
-        web2.find_element_by_xpath('//*[@id="sf_submit"]').click()
+
+        downloadTap.find_element_by_xpath('//*[@id="sf_submit"]').click()
         time.sleep(7)
-        web2.find_element_by_xpath('//*[@id="sf_result"]/div/div/div[2]/div[2]/div[1]').click()
+        # click on download button
+        downloadTap.find_element_by_xpath('//*[@id="sf_result"]/div/div/div[2]/div[2]/div[1]').click()
         time.sleep(1)
-        print(i)
+
+        # error
+        print(str(i+1)+" done")
     except:
-        print(str(one[i].get_attribute('href')).split('&list')[0])
+        print(listVideos[i])
+        errorListVideos.append(listVideos[i])
+        print(i+1)
+
+time.sleep(300)
+for i in range(len(errorListVideos)):
+    try:
+        downloadTap.get('https://ar.savefrom.net/' + errorListVideos[i])
+        time.sleep(2)
+
+        downloadTap.find_element_by_xpath('//*[@id="sf_submit"]').click()
+        time.sleep(7)
+        # click on download button
+        downloadTap.find_element_by_xpath('//*[@id="sf_result"]/div/div/div[2]/div[2]/div[1]').click()
+        time.sleep(1)
+
+        # error
+        print(str(i+1)+" done")
+    except:
+        print(errorListVideos[i])
+        print(i+1)
